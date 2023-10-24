2a36389bd315ca0db5aaeedede303fd6fc9af302
Zimuzo Akanne <127262688+Zim67@users.noreply.github.com>
2023-10-24 19:31:48 -0230

    Add files via upload

---
+++ Author.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Author {
+    private String name;
+    private String dateOfBirth;
+    private List<Book> booksWritten;
+
+    // Constructor
+    public Author(String name, String dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.booksWritten = new ArrayList<>();
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public List<Book> getBooksWritten() {
+        return booksWritten;
+    }
+
+    public void setBooksWritten(List<Book> booksWritten) {
+        this.booksWritten = booksWritten;
+    }
+}
